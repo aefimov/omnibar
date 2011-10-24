75476778afb81cc3ec44499e1fd66c727eb0562c
Alexey Efimov <aefimov-box@ya.ru>
2011-10-24 17:10:01 +0400

    Support for DuckDuckGo, WolframAlpha, Facebook and add HTTPS Option Also support for shortcuts to use not Default Search Engine.
    https://github.com/aefimov/omnibar/issues/2

--- OmniBar.safariextension/chrome/components/omnibar.js
+++ OmniBar.safariextension/chrome/components/omnibar.js
@@ -5,13 +5,65 @@
  */
 Omnibar = new function () {
     this.DEFAULT_EXTENSION_ID = "OmniBar.safariext";
-    this.DEFAULT_PATTERN = "http://yandex.ru/yandsearch" +
-            "?text=${query}" +
-            "&from=${extensionId}";
+    this.ENGINES = {
+        "yandex.ru": this.DEFAULT_ENGINE = {
+            shortcuts: ["y", "ya", "я"],
+            searchPattern: "yandex.ru/yandsearch?text=${query}&from=${extensionId}",
+            httpsAvailable: false,
+            group: "yandex"
+        },
+        "yandex.com": {
+            shortcuts: ["y", "ya"],
+            searchPattern: "yandex.com/yandsearch?text=${query}&from=${extensionId}",
+            httpsAvailable: false,
+            group: "yandex"
+        },
+        "google": {
+            shortcuts: ["g"],
+            searchPattern: "google.com/search?client=safari&q=${query}&ie=UTF-8&oe=UTF-8&from=${extensionId}",
+            httpsAvailable: true
+        },
+        "yahoo": {
+            shortcuts: ["y", "ya", "y!", "!"],
+            searchPattern: "search.yahoo.com/search?ei=utf-8&p=${query}&fr=${extensionId}",
+            httpsAvailable: false
+        },
+        "bing": {
+            shortcuts: ["b"],
+            searchPattern: "www.bing.com/search?q=${query}&from=${extensionId}",
+            httpsAvailable: false
+        },
+        "duckduckgo": {
+            shortcuts: ["d", "dd"],
+            searchPattern: "duckduckgo.com/?q=${query}&from=${extensionId}",
+            httpsAvailable: true
+        },
+        "wolframalpha": {
+            shortcuts: ["w", "wa"],
+            searchPattern: "wolframalpha.com/input/?i=${query}&from=${extensionId}",
+            httpsAvailable: false
+        },
+        "facebook": {
+            shortcuts: ["f", "fb"],
+            searchPattern: "facebook.com/search.php?q=${query}&from=${extensionId}",
+            httpsAvailable: true
+        }
+    };
 
     this.toSearchUrl = function (text) {
-        return Omnibar.Settings.getSearchPattern()
-                .replace("${query}", text)
+        var searchEngine = Omnibar.Settings.getSearchEngine();
+        var prefix = text.replace(/^(\w{1,2})\s.*$/, "$1").trim();
+        if (prefix.length > 0) {
+            // Try to find shortcut
+            var engine = Omnibar.Settings.findAlternateSearchEngine(prefix.toLocaleLowerCase(), searchEngine);
+            if (engine && engine != null) {
+                searchEngine = engine;
+                // Remove prefix from text, cos we found alternative search engine
+                text = text.replace(/^\w{1,2}\s+(.*)$/, "$1").trim();
+            }
+        }
+        return Omnibar.Settings.getSearchPattern(searchEngine)
+                .replace("${query}", text.trim())
                 .replace("${extensionId}", Omnibar.Settings.getExtensionId());
     };
 
@@ -31,11 +83,33 @@ Omnibar.Settings = new function() {
         }
         return extensionId;
     };
-    this.getSearchPattern = function () {
-        var searchPattern = safari.extension.settings.searchPattern;
-        if (searchPattern == null) {
-            return Omnibar.DEFAULT_PATTERN;
+
+    this.getSearchEngine = function() {
+        var engine = Omnibar.ENGINES[safari.extension.settings.searchEngineId];
+        if (!engine || engine == null) {
+            engine = Omnibar.DEFAULT_ENGINE;
         }
-        return searchPattern;
+        return engine;
+    };
+
+    this.findAlternateSearchEngine = function(shortcut, defaultEngine) {
+        // Look only all other engines, if user try to specify shortcut
+        for (var engineId in Omnibar.ENGINES) {
+            if (Omnibar.ENGINES.hasOwnProperty(engineId)) {
+                var engine = Omnibar.ENGINES[engineId];
+                var group = engine.group || "";
+                if (engine && engine !== defaultEngine &&
+                        group !== defaultEngine.group &&
+                        engine.shortcuts.indexOf(shortcut) != -1) {
+                    return engine;
+                }
+            }
+        }
+        return null;
+    };
+
+    this.getSearchPattern = function(engine) {
+        return (safari.extension.settings.useHttps && engine.httpsAvailable ? "https" : "http") +
+                "://" + engine.searchPattern;
     };
 }();
